9d18ab6be2431ea2ed5c7e54eb57119a087758fe
Alexey Melezhik <melezhik@gmail.com>
2016-11-14 23:00:45 +0300

    Update minoca.sh

--- minoca.sh
+++ minoca.sh
@@ -1,6 +1,6 @@
 wget --no-check-certificate https://cpanmin.us -o /usr/bin/cpanm && \
 chmod +x /usr/bin/cpanm && \
-ln -s /bin/env /usr/bin/env && \
+ln -fs /bin/env /usr/bin/env && \
 opkg -f /etc/opkg/opkg.conf update && \
 opkg -f /etc/opkg/opkg.conf  install perl bash curl make gcc && \
 cpanm --notest -q Sparrow && \
