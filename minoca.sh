1e2ed336aaa6fe85f697a057653c7015f5c6fcdc
Alexey Melezhik <melezhik@gmail.com>
2016-11-15 13:58:52 +0400

    Delete minoca.sh

--- minoca.sh
+++
@@ -1,7 +0,0 @@
-wget --no-check-certificate https://cpanmin.us -o /usr/bin/cpanm && \
-chmod +x /usr/bin/cpanm && \
-ln -fs /bin/env /usr/bin/env && \
-opkg -f /etc/opkg/opkg.conf update && \
-opkg -f /etc/opkg/opkg.conf  install perl bash curl make gcc && \
-cpanm --notest -q Sparrow && \
-sparrow index update
